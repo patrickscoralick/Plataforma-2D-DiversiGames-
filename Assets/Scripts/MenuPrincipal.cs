6a22a57c2342b374d539d6b5798b42fa15a5cdc6
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2024-09-16 11:44:38 -0300

    Upload Projeto
    A concluir a questão do level design

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void ComecarJogo(){
+        SceneManager.LoadScene("1 - Tutorial");
+    }
+
+    public void FecharJogo(){
+        Debug.Log("FECHAR O JOGO");
+        Application.Quit();
+    }
+}
